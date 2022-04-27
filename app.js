ccc6ee9dd62d1f6fd46dbfaad72e3a6cbb144632
Lukas Baur <baurlukas89@gmail.com>
2022-04-27 12:03:44 +0200

    Solved lab, bonus iteration 6 missing

--- app.js
+++ app.js
@@ -13,13 +13,35 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
-
 app.get('/', (req, res) => {
+  // send back content
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  // getBeers() response
+  punkAPI
+    .getBeers()
+    .then(beersFromAPI => {
+      res.render('beers', { beersFromAPI });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(randomBeer => {
+      res.render('random-beer', { randomBeer });
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('*', (req, res) => {
+  res.send(`Path not defined`);
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
